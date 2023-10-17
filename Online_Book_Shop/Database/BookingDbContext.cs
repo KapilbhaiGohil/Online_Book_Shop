bc05b73d855989cbe13844ce1414073dc36263a5
Kapilbhai Gohil <kapilbhaigohil@gmail.com>
2023-10-17 17:46:30 +0530

    Project Initialization

---
+++ Online_Book_Shop/Database/BookingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Online_Book_Shop.Database
+{
+    public class BookingDbContext:DbContext
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
